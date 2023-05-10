a9364d3ae0cde536cbd3af379019493491708196
Shivamani Bartaula <74212245+shivamanibrt@users.noreply.github.com>
2023-05-11 07:35:17 +1000

    Update Footer.js

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -4,7 +4,7 @@ export const Footer = () => {
   return (
     <div className="mt-5 bg-dark text-light p-5 text-center">
       &copy; Copy right reserved by Expenses Tracker | Made By{" "}
-      <a href="">Prem</a>
+      <a href="">Shivamani</a>
     </div>
   );
 };
